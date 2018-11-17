2bce2a1dd64bee31efc6b451027ad5f5fd7c98b4
chwostian <chwostian@post.pl>
2018-11-17 12:48:09 +0100

    2018-11-17 godz. 12:48

---
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -0,0 +1,10 @@
+package pl.coderslab.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+}
